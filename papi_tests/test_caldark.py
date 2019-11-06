f469f58cbf0334fbc45a55797fd0f528f3c791fa
JoseMiguel <jmiguel@iaa.es>
2019-11-06 09:20:18 +0100

    First test implementation

--- papi_tests/test_caldark.py
+++ papi_tests/test_caldark.py
@@ -1,5 +1,17 @@
 import pytest
 from reduce import calDark
 
+
+files = ["/home/jmiguel/DATA/PANIC2/test_files/Darks_N6_0106.join.fits",
+        "/home/jmiguel/DATA/PANIC2/test_files/Darks_N6_0107.join.fits",
+        "/home/jmiguel/DATA/PANIC2/test_files/Darks_N6_0108.join.fits"]
+
+temp_directory = "/tmp"
+
+
 def test_caldark():
-    cd = calDark.MasterDark()
+
+    cd = calDark.MasterDark(files, temp_dir=temp_directory)
+    out = cd.createMaster()
+    assert out == "/tmp/mdark_12_1.fits"
+
