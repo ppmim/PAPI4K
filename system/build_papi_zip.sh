ca0a9b7d3983c49639f2e59afb05bf294f9c4631
José Miguel (udit22) <jmiguel@iaa.es>
2022-12-14 11:20:28 +0100

    fix: script to build copy from source files

--- system/build_papi_zip.sh
+++ system/build_papi_zip.sh
@@ -1,7 +1,7 @@
 DEST=/tmp
-PAPI_DIR=../papi/
+PAPI_DIR=../
 now=`date +"%Y%m%d-%H%M%S"`
 cd $PAPI_DIR
-git archive --format zip --output $DEST/papi_$now.zip master
+git archive --format zip --output $DEST/papi_$now.zip master papi
 
 echo $DEST/papi_$now.zip 
