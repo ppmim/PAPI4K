384904dc52c6e11b49581e84d7fbb5ecb35fad8d
jmiguel @ udit22 <jmiguel@iaa.es>
2024-02-15 14:52:42 +0100

    Add new NLC to PAPI; no tested yet

--- system/build_papi_zip.sh
+++ system/build_papi_zip.sh
@@ -5,3 +5,7 @@ cd $PAPI_DIR
 git archive --format zip --output $DEST/papi_$now.zip master papi
 
 echo $DEST/papi_$now.zip 
+
+echo "Copying to panic22 and panic35...."
+rsync -av $DEST/papi_$now.zip panic@panic22.caha.es:/data1/ICS/QL_INSTALL/PAPI22
+rsync -av $DEST/papi_$now.zip panic@panic35.caha.es:/data1/ICS/QL_INSTALL/PAPI22
